dfc8751bcd4db0828e700f1c5313723f6a3b530e
Rik Smith-Unna <rik@fibrill.ae>
2021-10-01 19:48:55 +0100

    basic setup

--- site/src/components/app-root/app-root.tsx
+++ site/src/components/app-root/app-root.tsx
@@ -10,7 +10,7 @@ export class AppRoot {
       <ion-app>
         <ion-router useHash={false}>
           <ion-route url="/" component="app-home" />
-          <ion-route url="/profile/:name" component="app-profile" />
+          <ion-route url="/garden/:slug" component="app-garden" />
         </ion-router>
         <ion-nav />
       </ion-app>
